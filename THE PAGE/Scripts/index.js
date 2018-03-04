9371e0b2de38569c78efeba7821aba02cc4a8c65
Budge <budge@Budges-MacBook-Pro.local>
2018-03-03 17:30:49 -0700

    better mobile layout

--- THE PAGE/Scripts/index.js
+++ THE PAGE/Scripts/index.js
@@ -1,5 +1,12 @@
+var $ = function(id){   return document.getElementById(id); }
+
 function postClick(){ document.location.assign("posts.html"); }
 
 function tunesClick(){  document.location.assign("tunes.html"); }
 
 function resumeClick(){}
+
+function loadBackgroundImage()
+{
+    var bgImage = document.getElementsByClassName    
+}
